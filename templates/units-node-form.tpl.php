9175cd96599a1bd8fa46f9a2fc6186a7fded7e6c
Twila Camp <tcamp@ou.edu>
2016-12-12 19:07:58 -0600

    adjusting unit template to accomodate social media icons

--- templates/units-node-form.tpl.php
+++ templates/units-node-form.tpl.php
@@ -4,23 +4,35 @@
 communicate important information to the organization such as workflows, trainings, and documentation. </p>
 </div>
 
-<h3>General Information</h3>
+<h3>General</h3>
 <div class="content-border"> 
     <?php print drupal_render($form['title']); ?>
-    <?php print drupal_render($form['field_unit_email']); ?> 
-	<?php print drupal_render($form['field_unit_phone']); ?> 
     <?php print drupal_render($form['field_image']); ?> 
     <?php print drupal_render($form['field_description']); ?> 	
 </div>
-<h3>Documents & Links </h3>
 
-<p>TIP: To add documents, first upload them to Docs & Forms to get a shareable URL.</p>
+<h3>Contact Info</h3>
+<div class="content-border">
+	<?php print drupal_render($form['field_locations']); ?>
+	<?php print drupal_render($form['field_unit_phone']); ?>
+    <?php print drupal_render($form['field_unit_fax']); ?>
+	<?php print drupal_render($form['field_unit_email']); ?>	
+	<?php print drupal_render($form['field_facebook']); ?> 
+	<?php print drupal_render($form['field_twitter']); ?> 
+	<?php print drupal_render($form['field_instagram']); ?>
+    <?php print drupal_render($form['field_blog']); ?>  	
+</div>
+
+<h3>Useful Links </h3>
+<div class="content-border">
+<p>Help employees know how best to work with your unit by providing resources. Tip: Post files to the Docs & Forms site to get a sharable link.</p>
+<?php print drupal_render($form['field_section_header']); ?> 
 <?php print drupal_render($form['group_generalinfo']); ?> 
 </div>
 
-<h3>Projects & Initiatives</h3>
+<h3>Highlights</h3>
 <div class="content-border">
-<p style="margin-bottom:-10px;">Tell the organization a little more about the important work you do. TIP: use the photo collection to search for images to pair with your text.</p>
+<p style="margin-bottom:-10px;">Highlight a project, initative, featured space, or special collection. TIP: use the photo collection to search for images to pair with your text.</p>
 <?php print drupal_render($form['field_projects']); ?>
 </div>
 <?php print drupal_render_children($form);?>
